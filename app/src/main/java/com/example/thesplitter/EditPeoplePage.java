6b126a520db0fa4a532890a09e45c4e7d5e59894
meimei <5474@ait.nsw.edu.au>
2020-01-21 15:25:35 +1100

    Change back button to go to by item page

--- app/src/main/java/com/example/thesplitter/EditPeoplePage.java
+++ app/src/main/java/com/example/thesplitter/EditPeoplePage.java
@@ -23,7 +23,7 @@ public class EditPeoplePage extends AppCompatActivity {
         switch (item.getItemId()) {
             case android.R.id.home:
                 // app icon in action bar clicked; go home
-                Intent intent = new Intent(this, Home.class);
+                Intent intent = new Intent(this, ByItemPage.class);
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(intent);
                 return true;
